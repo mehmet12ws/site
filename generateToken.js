52b92aae5a473f48f9f56b5c8d92b1491b8a94de
mehmet12ws <126833226+mehmet12ws@users.noreply.github.com>
2024-08-10 20:17:38 +0300

    Update generateToken.js

--- generateToken.js
+++ generateToken.js
@@ -1,7 +1,8 @@
+const express = require('express');
 const jwt = require('jsonwebtoken');
-const Buffer = require('buffer').Buffer;
+const app = express();
 
-const secret = 'your-256-bit-secret'; // Güvenli bir şekilde saklayın
+const secret = 'your-256-bit-secret'; // Aynı gizli anahtarı kullanın
 
 // Base64 ile kodlanmış şifre
 const base64Password = 'WHg0NDI0WHM0NGR8'; // 
@@ -16,5 +17,12 @@ const token = jwt.sign(payload, secret, { expiresIn: '1h' }); // Token 1 saat ge
 // Token'ı Base64 ile şifreleme
 const base64Token = Buffer.from(token).toString('base64');
 
-console.log('JWT Token:', token);
-console.log('Base64 Encoded JWT Token:', base64Token);
+app.get('/get-token', (req, res) => {
+    res.json({ token: base64Token });
+});
+
+app.use(express.static('public')); // 'public' klasöründen dosyalar sunulacak
+
+app.listen(3000, () => {
+    console.log('Sunucu çalışıyor');
+});
