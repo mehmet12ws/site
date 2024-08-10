c82e73f2b937377bc3439f65d9f2932118467be6
mehmet12ws <126833226+mehmet12ws@users.noreply.github.com>
2024-08-11 00:41:25 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const bodyParser = require('body-parser');
-const axios = require('axios'); // Turnstile doğrulama için gerekli
+const axios = require('axios');
 const app = express();
 
 const secret = 'your-256-bit-secret'; // Gizli anahtar
@@ -15,7 +15,7 @@ app.post('/generate-token', (req, res) => {
     if (password && turnstileToken) {
         // Şifreyi JWT token'a dönüştür
         const token = jwt.sign({ password }, secret, { expiresIn: '1h' }); // Token 1 saat geçerli
-        res.json({ password: token, turnstileToken });
+        res.json({ token });
     } else {
         res.status(400).json({ message: 'Şifre ve Turnstile tokenı sağlanmalıdır.' });
     }
@@ -23,13 +23,13 @@ app.post('/generate-token', (req, res) => {
 
 // JWT token doğrulama endpoint'i
 app.post('/login', async (req, res) => {
-    const { password, turnstileToken } = req.body;
+    const { token, turnstileToken } = req.body;
 
     try {
         // Turnstile token'ını doğrulama
         const turnstileResponse = await axios.post('https://challenges.cloudflare.com/turnstile/v0/siteverify', null, {
             params: {
-                secret: '0x4AAAAAAAg6TDS6kf69wBpjwnJ6cZ7Tq-8', // Turnstile'ın secret anahtarını buraya ekleyin
+                secret: '0x4AAAAAAAg6TDS6kf69wBpjwnJ6cZ7Tq-8',
                 response: turnstileToken
             }
         });
@@ -39,14 +39,14 @@ app.post('/login', async (req, res) => {
         }
 
         // JWT token'ı doğrulama
-        const decoded = jwt.verify(password, secret);
+        const decoded = jwt.verify(token, secret);
         const passwordFromToken = decoded.password;
 
-        // Şifreyi kontrol et
-        if (passwordFromToken === 'asdasdad') {
+        // Token'dan çıkan şifreyi doğrulama
+        if (passwordFromToken) {
             res.json({ message: 'Giriş başarılı' });
         } else {
-            res.status(401).json({ message: 'Geçersiz şifre' });
+            res.status(401).json({ message: 'Geçersiz token' });
         }
     } catch (err) {
         res.status(401).json({ message: 'Geçersiz token' });
