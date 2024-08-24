23296a7cdae89a83a2ad2f7be89cc8098e290710
mehmet12ws <126833226+mehmet12ws@users.noreply.github.com>
2024-08-25 01:16:48 +0300

    Update and rename 31.py to app.py

--- app.py
+++ app.py
@@ -1,16 +1,7 @@
-from flask import Flask, render_template, request, jsonify
-import requests
-import time
-
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return render_template('deneme.html')
-
 @app.route('/send_request', methods=['POST'])
 def send_request():
     api_key = request.form.get('apiKey')
+    print(f"Received API Key: {api_key}")
 
     # 2Captcha API URL'leri
     create_task_url = "https://2captcha.com/in.php"
@@ -25,12 +16,15 @@ def send_request():
         "json": 1
     }
 
-    # İsteği gönderme
     response = requests.post(create_task_url, data=request_body)
+    print(f"Create Task Response: {response.text}")
+    
     result = response.json()
+    print(f"Parsed JSON Result: {result}")
 
     if result.get("status") == 1:
         task_id = result.get("request")
+        print(f"Task ID: {task_id}")
 
         # Sonucu kontrol etmek için biraz bekleme (örneğin 20 saniye)
         time.sleep(20)
@@ -43,14 +37,12 @@ def send_request():
             "json": 1
         }
         result_response = requests.get(get_result_url, params=result_params)
+        print(f"Result Response: {result_response.text}")
+        
         result_data = result_response.json()
-
         if result_data.get("status") == 1:
             return jsonify({"success": True, "gRecaptchaResponse": result_data.get("request")})
         else:
             return jsonify({"success": False, "message": result_data.get("request")})
     else:
         return jsonify({"success": False, "message": result.get("request")})
-
-if __name__ == '__main__':
-    app.run(debug=True)
